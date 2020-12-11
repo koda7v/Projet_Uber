2b9a174adc5f7a7d8a7d28b490feca114af88262
EPE <pech.erwan@hotmail.com>
2020-12-11 15:22:04 +0100

    Ajout Back et Front

---
+++ Back/src/main/java/MYE/Projet_Uber/ProjetUberApplication.java
@@ -0,0 +1,13 @@
+package MYE.Projet_Uber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetUberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetUberApplication.class, args);
+	}
+
+}
---
+++ Back/src/test/java/MYE/Projet_Uber/ProjetUberApplicationTests.java
@@ -0,0 +1,13 @@
+package MYE.Projet_Uber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetUberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
