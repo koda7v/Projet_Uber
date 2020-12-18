eaee2185ecc6d664e05462c6fbba2cbd00d001dd
Houdini <yannlebris97@gmail.com>
2020-12-18 16:57:24 +0100

    Début du model du back

---
+++ Back/src/main/java/model/Client.java
@@ -0,0 +1,130 @@
+package model;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import org.springframework.data.annotation.Id;
+
+/**
+ * @author yannl
+ *
+ */
+public class Client
+{
+
+  @Id
+  protected Long id;
+
+  @NotEmpty
+  protected String nom;
+
+  @NotEmpty
+  protected String prenom;
+
+  @NotEmpty
+  protected String adresse;
+
+  @NotEmpty
+  protected String email;
+
+  @NotEmpty(message = "Votre numero de telephone doit avoir des chiffres")
+  @Size(min = 10, max = 12, message = "Votre numero de telephone doit avoir entre 10 et 12 chiffres")
+  protected String telephone;
+
+  /**
+   * 
+   * @param id
+   * @param nom
+   * @param prenom
+   * @param adresse
+   * @param email
+   * @param telephone
+   */
+  public Client(Long id, String nom, String prenom, String adresse, String email, String telephone)
+  {
+    this.id = id;
+    this.nom = nom;
+    this.prenom = prenom;
+    this.adresse = adresse;
+    this.email = email;
+    this.telephone = telephone;
+  }
+
+  /**
+   * 
+   * @param nom
+   * @param prenom
+   * @param adresse
+   * @param email
+   * @param telephone
+   */
+  public Client(String nom, String prenom, String adresse, String email, String telephone)
+  {
+    this.nom = nom;
+    this.prenom = prenom;
+    this.adresse = adresse;
+    this.email = email;
+    this.telephone = telephone;
+  }
+
+  public Long getId()
+  {
+    return id;
+  }
+
+  public void setId(Long id)
+  {
+    this.id = id;
+  }
+
+  public String getNom()
+  {
+    return nom;
+  }
+
+  public void setNom(String nom)
+  {
+    this.nom = nom;
+  }
+
+  public String getPrenom()
+  {
+    return prenom;
+  }
+
+  public void setPrenom(String prenom)
+  {
+    this.prenom = prenom;
+  }
+
+  public String getAdresse()
+  {
+    return adresse;
+  }
+
+  public void setAdresse(String adresse)
+  {
+    this.adresse = adresse;
+  }
+
+  public String getEmail()
+  {
+    return email;
+  }
+
+  public void setEmail(String email)
+  {
+    this.email = email;
+  }
+
+  public String getTelephone()
+  {
+    return telephone;
+  }
+
+  public void setTelephone(String telephone)
+  {
+    this.telephone = telephone;
+  }
+
+}
