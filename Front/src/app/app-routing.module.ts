a19c6b3ea87e5bc3b104f7a5cef276f54b3c0484
Houdini <yannlebris97@gmail.com>
2021-02-04 20:25:27 +0100

    Premier jet pour la connexion sur le front, encore plusieurs choses à faire pour coller à l'application

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -2,6 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { NotFoundComponent } from 'src/app/layout/not-found/not-found.component';
+import { RegisterComponent } from './jwt/register/register.component';
+import { LoginComponent } from './jwt/login/login.component';
+import { HomeComponent } from './jwt/home/home.component';
+import { ProfileComponent } from './jwt/profile/profile.component';
+import { BoardUserComponent } from './jwt/board-user/board-user.component';
+import { BoardModeratorComponent } from './jwt/board-moderator/board-moderator.component';
+import { BoardAdminComponent } from './jwt/board-admin/board-admin.component';
 
 const routes: Routes = [
   {
@@ -14,6 +21,15 @@ const routes: Routes = [
     loadChildren: () =>
       import('./core/plat/plat.module').then((m) => m.PlatModule)
   },
+  
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'user', component: BoardUserComponent },
+  { path: 'mod', component: BoardModeratorComponent },
+  { path: 'admin', component: BoardAdminComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '404', component: NotFoundComponent },
   { path: '**', redirectTo: '/404' }
 ];
--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule, LOCALE_ID } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { DomSanitizer } from '@angular/platform-browser';
 import localeFr from '@angular/common/locales/fr';
 
@@ -10,22 +10,44 @@ import { MatIconRegistry } from '@angular/material/icon';
 import { MatPaginatorIntl } from '@angular/material/paginator';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { AppLayoutModule } from './layout/app-layout.module';
 import { paginatorFrench } from './material-intl/paginator-fr';
+import { LoginComponent } from './jwt/login/login.component';
+import { RegisterComponent } from './jwt/register/register.component';
+import { HomeComponent } from './jwt/home/home.component';
+import { ProfileComponent } from './jwt/profile/profile.component';
+import { BoardAdminComponent } from './jwt/board-admin/board-admin.component';
+import { BoardModeratorComponent } from './jwt/board-moderator/board-moderator.component';
+import { BoardUserComponent } from './jwt/board-user/board-user.component';
+
+import { authInterceptorProviders } from './jwt/_helpers/auth.interceptor';
 
 registerLocaleData(localeFr, 'fr');
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ProfileComponent,
+    BoardAdminComponent,
+    BoardModeratorComponent,
+    BoardUserComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    AppLayoutModule
+    AppLayoutModule,
+    FormsModule
   ],
   providers: [
+    authInterceptorProviders,
     { provide: LOCALE_ID, useValue: 'fr' },
     { provide: MatPaginatorIntl, useClass: paginatorFrench }
   ],
